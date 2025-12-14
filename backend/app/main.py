2d96b87699b8efecb78eedf57f6faa021e240ba9
Gagan Sarmah <sarmahgagan58@gmail.com>
2025-12-14 16:17:19 +0530

    add cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .database import Base, engine
 from . import models
 from .routers import auth,sweets,inventory
+from fastapi.middleware.cors import CORSMiddleware
 
 try:
     Base.metadata.create_all(bind=engine)
@@ -10,6 +11,14 @@ except Exception as e:
     print(f"Error creating tables: {e}")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def message():
